503f6ee2acc33278e225dee57708f3ea021f6b60
Davi Almeida de Lima <contatoalmeidadavi@gmail.com>
2025-10-10 15:12:42 -0300

    Adiciona arquivo de configuração da API

---
+++ config/api.js
@@ -0,0 +1,3 @@
+export const API_URL = "http://localhost:3000/api";
+// Se for testar no celular físico, troque "localhost" pelo IP local do seu computador.
+// Exemplo: export const API_URL = "http://192.168.0.5:3000/api";
